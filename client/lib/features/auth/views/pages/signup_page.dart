6fe8b1c1974076a0f45d348caf98cb1af42534c0
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-16 12:18:29 +0800

    Implements static signup page UI

--- client/lib/features/auth/views/pages/signup_page.dart
+++ client/lib/features/auth/views/pages/signup_page.dart
@@ -1,3 +1,6 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/features/auth/views/widgets/auth_gradient_button.dart';
+import 'package:client/features/auth/views/widgets/custom_field.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -10,6 +13,48 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign Up',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 32),
+            const CustomField(
+              hintText: 'Name',
+            ),
+            const SizedBox(height: 16),
+            const CustomField(
+              hintText: 'Email',
+            ),
+            const SizedBox(height: 16),
+            const CustomField(hintText: 'Password'),
+            const SizedBox(height: 20),
+            const AuthGradientButton(),
+            const SizedBox(height: 20),
+            RichText(
+              text: TextSpan(
+                text: 'Already have an account? ',
+                style: Theme.of(context).textTheme.titleMedium,
+                children: const [
+                  TextSpan(
+                    text: 'Sign in',
+                    style: TextStyle(
+                      color: Pallete.gradient2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ client/lib/features/auth/views/widgets/auth_gradient_button.dart
@@ -0,0 +1,35 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AuthGradientButton extends StatelessWidget {
+  const AuthGradientButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            Pallete.gradient1,
+            Pallete.gradient2,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(395, 55),
+          backgroundColor: Pallete.transparentColor,
+          shadowColor: Pallete.transparentColor,
+        ),
+        child: const Text(
+          'Sign up',
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
