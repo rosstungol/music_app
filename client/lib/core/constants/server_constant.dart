1278cb52a5b8a9b22d38d694a3c5e23f87cf0d83
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-21 12:07:33 +0800

    Adds server URL condition for Android emulator

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 class ServerConstant {
-  // 10.0.2.2 is an AVD alias to localhost
-  static const String serverURL = "http://10.0.2.2:8000";
+  static String serverURL =
+      // 10.0.2.2 is an AVD alias to localhost for Android emulator
+      Platform.isAndroid ? "http://10.0.2.2:8000" : "http://127.0.0.1:8000";
 }
