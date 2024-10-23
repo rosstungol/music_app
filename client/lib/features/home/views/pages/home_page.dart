5202c9d9c10540bb43fce2d25bd9b8406feab78a
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-23 13:38:20 +0800

    Adds empty home page

---
+++ client/lib/features/home/views/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/providers/current_user_notifier.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(currentUserNotifierProvider);
+    print(user);
+
+    return const Scaffold();
+  }
+}
