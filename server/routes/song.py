dfdd360f50aa00cb83408901c6a05d4e6daca0ae
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-29 20:39:44 +0800

    Implements song upload feature on song route

--- server/routes/song.py
+++ server/routes/song.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 from middleware.auth_middleware import auth_middleware
 from dotenv import load_dotenv
+from models.song import Song
 import cloudinary
 import cloudinary.uploader
 import os
@@ -12,14 +13,14 @@ load_dotenv()
 
 router = APIRouter()
 
-cloudinary.config( 
-    cloud_name = "dkuxxge3j", 
-    api_key = "733918919263336", 
+cloudinary.config(
+    cloud_name = "dkuxxge3j",
+    api_key = "733918919263336",
     api_secret = os.getenv('CLOUDINARY_API_SECRET_KEY'),
     secure=True
 )
 
-@router.post('/upload')
+@router.post('/upload', status_code=201)
 def upload_song(song: UploadFile = File(...),
                 thumbnail: UploadFile =  File(...),
                 artist: str = Form(...),
@@ -27,11 +28,21 @@ def upload_song(song: UploadFile = File(...),
                 hex_code: str = Form(...),
                 db: Session = Depends(get_db),
                 auth_dict = Depends(auth_middleware)):
-  song_id = str(uuid.uuid4())
-  song_res = cloudinary.uploader.upload(song.file, resource_type='auto', folder=f'songs/{song_id}')
-  print(song_res)
+    song_id = str(uuid.uuid4())
+    song_res = cloudinary.uploader.upload(song.file, resource_type='auto', folder=f'songs/{song_id}')
+    thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
 
-  thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
-  print(thumbnail_res)
+    new_song = Song(
+        id=song_id,
+        song_name=song_name,
+        artist=artist,
+        hex_code=hex_code,
+        song_url=song_res['url'],
+        thumbnail_url=thumbnail_res['url'],
+    )
 
-  return 'ok'
+    db.add(new_song)
+    db.commit()
+    db.refresh(new_song)
+
+    return new_song
